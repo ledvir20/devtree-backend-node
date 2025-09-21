b4dfeaf757aa0a49a40bcff701099062b2c10770
Ledvir Anthony Bautista Perez <ledvir.bautista.27@unsch.edu.pe>
2025-09-21 17:15:29 -0500

    Implementar registro de usuario en la ruta de autenticación

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import User from "./models/user.model";
 
 const router = Router();
 
 // Autentificacion y registro
-router.post("/auth/register", (req, res) => {
-  res.send("Welcome to the Registration Page!");
+router.post("/auth/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).send("User registered successfully");
+  } catch (error) {
+    res.status(400).send("Error registering user");
+  }
 });
 
 export default router;
