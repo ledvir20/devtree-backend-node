e5ae6822fd56cd7cc5568385186d9ec2efa81e29
Ledvir Anthony Bautista Perez <ledvir.bautista.27@unsch.edu.pe>
2025-09-21 18:07:36 -0500

    feat: add password hashing utility using bcrypt

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,22 @@
 import type { Request, Response } from "express";
 import User from "../models/user.model";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error = new Error("El usuario ya existe");
+      return res.status(409).json({ message: error.message });
+    }
+
     const user = new User({ name, email, password });
+
+    const hashedPassword = await hashPassword(password);
+    user.password = hashedPassword;
+
     await user.save();
 
     res.status(201).send("User registered successfully");
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createAccount } from "./handlers";
 
-const router = Router();
+const router: Router = Router();
 
 // Autentificacion y registro
 router.post("/auth/register", createAccount);
