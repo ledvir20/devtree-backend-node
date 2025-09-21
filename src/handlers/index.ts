572fa19d13cb509e07c93dae7cf40ffe52de21df
Ledvir Anthony Bautista Perez <ledvir.bautista.27@unsch.edu.pe>
2025-09-21 18:31:31 -0500

    feat: agregar validaciones de entrada en el registro de usuario utilizando express-validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,9 +2,16 @@ import type { Request, Response } from "express";
 import User from "../models/user.model";
 import { hashPassword } from "../utils/auth";
 import slug from "slug";
+import { validationResult } from "express-validator";
 
 export const createAccount = async (req: Request, res: Response) => {
   try {
+    // Manejo de errores y validaciones
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { createAccount } from "./handlers";
+import { body } from "express-validator";
 
 const router: Router = Router();
 
 // Autentificacion y registro
-router.post("/auth/register", createAccount);
+router.post(
+  "/auth/register",
+  body("handle").isString().notEmpty().withMessage("El handle es obligatorio"),
+  body("name").isString().notEmpty().withMessage("El nombre es obligatorio"),
+  body("email")
+    .isEmail()
+    .withMessage("Se requiere un correo electrónico válido"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe tener al menos 6 caracteres"),
+  createAccount
+);
 
 export default router;
